7d211e3928a4e67fb97e465cc4ac2512aaf1247b
RyuSeiri <lishilongjp@gmail.com>
2024-03-22 09:04:31 +0900

    update

---
+++ _locales/bat.bat
@@ -0,0 +1,110 @@
+mkdir am
+mkdir ar
+mkdir bg
+mkdir bn
+mkdir ca
+mkdir cs
+mkdir da
+mkdir de
+mkdir el
+mkdir en
+mkdir en_GB
+mkdir en_US
+mkdir es
+mkdir es_419
+mkdir et
+mkdir fa
+mkdir fi
+mkdir fil
+mkdir fr
+mkdir gu
+mkdir he
+mkdir hi
+mkdir hr
+mkdir hu
+mkdir id
+mkdir it
+mkdir ja
+mkdir kn
+mkdir ko
+mkdir lt
+mkdir lv
+mkdir ml
+mkdir mr
+mkdir ms
+mkdir nl
+mkdir no
+mkdir pl
+mkdir pt_BR
+mkdir pt_PT
+mkdir ro
+mkdir ru
+mkdir sk
+mkdir sl
+mkdir sr
+mkdir sv
+mkdir sw
+mkdir ta
+mkdir te
+mkdir th
+mkdir tr
+mkdir uk
+mkdir vi
+mkdir zh_CN
+mkdir zh_TW
+
+copy messages.json am
+copy messages.json ar
+copy messages.json bg
+copy messages.json bn
+copy messages.json ca
+copy messages.json cs
+copy messages.json da
+copy messages.json de
+copy messages.json el
+copy messages.json en
+copy messages.json en_GB
+copy messages.json en_US
+copy messages.json es
+copy messages.json es_419
+copy messages.json et
+copy messages.json fa
+copy messages.json fi
+copy messages.json fil
+copy messages.json fr
+copy messages.json gu
+copy messages.json he
+copy messages.json hi
+copy messages.json hr
+copy messages.json hu
+copy messages.json id
+copy messages.json it
+copy messages.json ja
+copy messages.json kn
+copy messages.json ko
+copy messages.json lt
+copy messages.json lv
+copy messages.json ml
+copy messages.json mr
+copy messages.json ms
+copy messages.json nl
+copy messages.json no
+copy messages.json pl
+copy messages.json pt_BR
+copy messages.json pt_PT
+copy messages.json ro
+copy messages.json ru
+copy messages.json sk
+copy messages.json sl
+copy messages.json sr
+copy messages.json sv
+copy messages.json sw
+copy messages.json ta
+copy messages.json te
+copy messages.json th
+copy messages.json tr
+copy messages.json uk
+copy messages.json vi
+copy messages.json zh_CN
+copy messages.json zh_TW
+pause
