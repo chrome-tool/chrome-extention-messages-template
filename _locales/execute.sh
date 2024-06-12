5199b02250f66d9c82dbd2ccd1d6537dbfa23a3d
RyuSeiri <lishilongjp@gmail.com>
2024-06-12 09:21:22 +0900

    Add files via upload

---
+++ _locales/execute.sh
@@ -0,0 +1,109 @@
+mkdir am
+mkdir ar
+mkdir bg
+mkdir bn
+mkdir ca
+mkdir cs
+mkdir da
+mkdir de
+mkdir el
+mkdir en
+mkdir en_GB
+mkdir en_US
+mkdir es
+mkdir es_419
+mkdir et
+mkdir fa
+mkdir fi
+mkdir fil
+mkdir fr
+mkdir gu
+mkdir he
+mkdir hi
+mkdir hr
+mkdir hu
+mkdir id
+mkdir it
+mkdir ja
+mkdir kn
+mkdir ko
+mkdir lt
+mkdir lv
+mkdir ml
+mkdir mr
+mkdir ms
+mkdir nl
+mkdir no
+mkdir pl
+mkdir pt_BR
+mkdir pt_PT
+mkdir ro
+mkdir ru
+mkdir sk
+mkdir sl
+mkdir sr
+mkdir sv
+mkdir sw
+mkdir ta
+mkdir te
+mkdir th
+mkdir tr
+mkdir uk
+mkdir vi
+mkdir zh_CN
+mkdir zh_TW
+
+cp messages.json am
+cp messages.json ar
+cp messages.json bg
+cp messages.json bn
+cp messages.json ca
+cp messages.json cs
+cp messages.json da
+cp messages.json de
+cp messages.json el
+cp messages.json en
+cp messages.json en_GB
+cp messages.json en_US
+cp messages.json es
+cp messages.json es_419
+cp messages.json et
+cp messages.json fa
+cp messages.json fi
+cp messages.json fil
+cp messages.json fr
+cp messages.json gu
+cp messages.json he
+cp messages.json hi
+cp messages.json hr
+cp messages.json hu
+cp messages.json id
+cp messages.json it
+cp messages.json ja
+cp messages.json kn
+cp messages.json ko
+cp messages.json lt
+cp messages.json lv
+cp messages.json ml
+cp messages.json mr
+cp messages.json ms
+cp messages.json nl
+cp messages.json no
+cp messages.json pl
+cp messages.json pt_BR
+cp messages.json pt_PT
+cp messages.json ro
+cp messages.json ru
+cp messages.json sk
+cp messages.json sl
+cp messages.json sr
+cp messages.json sv
+cp messages.json sw
+cp messages.json ta
+cp messages.json te
+cp messages.json th
+cp messages.json tr
+cp messages.json uk
+cp messages.json vi
+cp messages.json zh_CN
+cp messages.json zh_TW
